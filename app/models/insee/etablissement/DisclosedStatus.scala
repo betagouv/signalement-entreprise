93ad36fc429818ee0963a800197e21db5ea4669f
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-05-24 14:15:52 +0200

    [TRELLO-1787] Add new disclosed status 'P' (replacing 'N') (#15)

--- app/models/insee/etablissement/DisclosedStatus.scala
+++ app/models/insee/etablissement/DisclosedStatus.scala
@@ -9,7 +9,11 @@ object DisclosedStatus extends PlayEnum[DisclosedStatus] {
 
   val values = findValues
   case object Public extends DisclosedStatus("O")
-  case object NonPublic extends DisclosedStatus("N")
+  // Legacy
+  // https://sirene.fr/static-resources/htm/v_sommaire.htm#diffusionpartielle
+  // https://sirene.fr/static-resources/htm/v_sommaire.htm#72
+  case object NonPublicLegacy extends DisclosedStatus("N")
+  case object NonPublic extends DisclosedStatus("P")
 
   implicit val DisclosedStatusColumnType = MappedColumnType.base[DisclosedStatus, String](
     _.entryName,
